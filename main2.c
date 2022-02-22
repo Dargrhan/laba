fbb7601df044f27e17a8b23fbf8f9c7551d45881
Darya Rodina <dariagrhan@gmail.com>
2022-02-22 10:31:12 +0300

    change type kind of sorting, create array generation, variable names changed

--- main2.c
+++ main2.c
@@ -1,61 +1,51 @@
 #include  <stdlib.h>
 #include  <stdio.h>
-void Sort(int *arr, int n, int *count2)
+#include <math.h>
+void Sort(int *arr, int n, int *swaps, int *comparisons)
 {
   int m, med; 
   for (int i = 0; i < n - 1; i++) 
   {
     m = i; 
-    count2[1]++;
-    count2[1]++;
     for (int j = i + 1; j < n; j++)
     {
-      count2[1]+=2; //сравнение в цикле и ниже
-      if (arr[j] < arr[m]) {
+      comparisons[0]++;
+      if (abs(arr[j]) < abs(arr[m])) {
         m = j;   
-        count2[0]++;
       }
     }
-    count2[1]++;//несколько if не пройдут в цикл. но все равно сравнение будет
-    count2[1]++;
-    count2[1]++;
     med = arr[i];
-    count2[0]++;
+    swaps[0]++;
     arr[i] = arr[m];
-    count2[0]++;
+    swaps[0]++;
     arr[m] = med;
   }
 }
-void quickSort(int *arr, int left, int right, int *count1)
+void quickSort(int *arr, int left, int right, int *swaps, int *comparisons)
 {
   int k; 
   int l = left;  
   int r = right;
-  k = arr[left];
+  k = (arr[left]);
   while (left < right)
   {
-    count1[1]++;
-    while ((arr[right] >= k) && (left < right)){
-      count1[1]+=2;
+    while (abs(arr[right]) >= abs(k) && (left < right)){
+      comparisons[1]+=1;
       right--;
     }
-    count1[1]++;//тк если в цикл не прошло => сравнение выполнилось
-    count1[1]++;
     if (left != right) 
     {
-      count1[0]++;
+      swaps[1]++;
       arr[left] = arr[right]; 
       left++; 
     }
-    while ((arr[left] <= k) && (left < right)){
+    while (abs(arr[left]) <= abs(k) && (left < right)){
       left++; 
-      count1[1]+=2;
+      comparisons[1]+=1;
     }
-    count1[1]++;
-    count1[1]++;
     if (left != right) 
     {
-      count1[0]++;
+      swaps[1]++;
       arr[right] = arr[left]; 
       right--; 
     }
@@ -64,41 +54,103 @@ void quickSort(int *arr, int left, int right, int *count1)
   k = left;
   left = l;
   right = r;
-  count1[1]++;
   if (left < k) 
-    quickSort(arr, left, k - 1, count1);
-  count1[1]++;
+    quickSort(arr, left, k - 1, swaps, comparisons);
   if (right > k)
-    quickSort(arr, k + 1, right, count1);
+    quickSort(arr, k + 1, right, swaps, comparisons);
 }
 int main (void){
-    int Arr[20];
-    int Arr2[20];
-    int count1[5];
-    count1[0] = 0;
-    count1[1] = 0;
-    int count2[5]; //счетчик (1 - колво сравнений. 0 элемент - перестановок)
-    count2[0] = 0;
-    count2[1] = 0;
-    int n = 10;
-    int i;
-    for (int i = 0; i<n; i++) {
-        Arr[i] = rand() % 201 - 100;
-        Arr2[i] = Arr[i];
-        printf("%d", Arr[i]);
+    int Arr[20000];
+    int Arr2[20000];
+    int n;
+    int comparisons[5];
+    comparisons[0] = 0;
+    comparisons[1] = 0;
+    int swaps[5]; //счетчик (0 - sort. 1 элемент - quick sort)
+    swaps[0] = 0;
+    swaps[1] = 0;
+    for (n = 10; n<=10000; n = n*10){
+        int i;
+        for (int i = 0; i<n; i++) {
+            Arr[i] = rand() % 201 - 100;
+            Arr2[i] = Arr[i];
+            //printf("%d ", Arr[i]);
+        }
         printf("\n");
+        int left = 0;
+        int right = n - 1;
+        quickSort(Arr, left, right, swaps, comparisons);
+        Sort(Arr2, n, swaps, comparisons);
+        for ( int i = 0; i < n; i ++){
+            //printf("%d ", Arr[i]);
+        }
+        //printf("\n");
+        for ( int i = 0; i < n; i ++){
+            //printf("%d ", Arr2[i]);
+        }
+        printf("\n");
+        printf("%d %d ", swaps[0], comparisons[0]);
+        printf("%d %d", swaps[1], comparisons[1]);
     }
-    int left = 0;
-    int right = n - 1;
-    quickSort(Arr, left, right, count1);
-    Sort(Arr2, n, count2);
-    for ( int i = 0; i < n; i ++)
-        printf("%d ", Arr[i]);
+    printf("rand\n");
+    
+    comparisons[0] = 0;
+    comparisons[1] = 0; 
+    swaps[0] = 0;
+    swaps[1] = 0;
+    for (n = 10; n<=10000; n = n*10){
+        int i;
+        for (int i = 0; i<n; i++) {
+            Arr[i] = i;
+            Arr2[i] = Arr[i];
+            //printf("%d ", Arr[i]);
+        }
         printf("\n");
-    for ( int i = 0; i < n; i ++)
-        printf("%d ", Arr2[i]);
+        int left = 0;
+        int right = n - 1;
+        quickSort(Arr, left, right, swaps, comparisons);
+        Sort(Arr2, n, swaps, comparisons);
+        for ( int i = 0; i < n; i ++){
+            //printf("%d ", Arr[i]);
+        }
         printf("\n");
-    printf("%d %d ", count2[0], count2[1]);
-    printf("%d %d", count1[0], count1[1]);
+        for ( int i = 0; i < n; i ++){
+            //printf("%d ", Arr2[i]);
+        }
+        printf("\n");
+        printf("%d %d ", swaps[0], comparisons[0]);
+        printf("%d %d ", swaps[1], comparisons[1]);
+    }
+    printf("voz\n");
+    
+    
+    comparisons[0] = 0;
+    comparisons[1] = 0; 
+    swaps[0] = 0;
+    swaps[1] = 0;
+    for (n = 10; n<=10000; n = n*10){
+        int i;
+        for (int i = n-1; i>=0; i--) {
+            Arr[i] = i;
+            Arr2[i] = Arr[i];
+            //printf("%d ", Arr[i]);
+        }
+        printf("\n");
+        int left = 0;
+        int right = n - 1;
+        quickSort(Arr, left, right, swaps, comparisons);
+        Sort(Arr2, n, swaps, comparisons);
+        for ( int i = 0; i < n; i ++){
+            //printf("%d ", Arr[i]);
+        }
+        printf("\n");
+        for ( int i = 0; i < n; i ++){
+            //printf("%d ", Arr2[i]);
+        }
+        printf("\n");
+        printf("%d %d ", swaps[0], comparisons[0]);
+        printf("%d %d ", swaps[1], comparisons[1]);
+    }
+    printf("ub\n");
     return 0;
 }
